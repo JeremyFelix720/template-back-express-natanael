cc57b8935089c34598a1d3392e0bdb3fe99adf3c
jeremy <jeremyfelix41@gmail.com>
2024-01-04 15:49:45 +0100

    :heavy_plus_sign: add sequelize, dotenv, pg, nodemon, body-parser dependancies + sequelize code in index.ts + creation of gitignore file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,50 @@
 import express from "express"
 import "dotenv/config"
 import cors from 'cors'
+import bodyParser from "body-parser";
+import { Sequelize } from 'sequelize';  // Voir : https://sequelize.org/docs/v6/getting-started/
 
-const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3000
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
+const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3000
+const database = process.env.DATABASE as string;
+const username = process.env.USERNAME as string;
+const password = process.env.PASSWORD as string;
+const server = process.env.SERVER as string;
 
-app.get('/hello', (req, res) => {
-    res.send('hello world');
+const sequelize = new Sequelize(database, username, password, {
+  host: server,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    },
+    dialectModule: require('pg'),
+});
+
+async function connexionTest() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+connexionTest();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/toto', (req, res) => {
+    res.send('Toto');
   });
 
   app.listen(port, () => {
